4a4fefae6aaecfc6f178bc73c12030ed41aad161
zolo1917 <adwaith_venkatesh@msn.com>
2021-12-16 11:11:46 +0530

    added Carousel

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Creatd by @zolo1917
+ * 
+ */
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
